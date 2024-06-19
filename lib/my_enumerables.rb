a60d350b7ba0f22689924bd1e59c6b77267f8fc2
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-19 11:56:07 +0800

    Done Enumerables Exercises

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,6 +8,70 @@ module Enumerable
       index += 1
     end
   end
+
+  def my_select
+    arr = Array.new
+    for element in self
+      arr.push(element) if yield(element)
+    end
+
+    arr
+  end
+
+  def my_all?
+    for element in self
+      return false unless yield(element)
+    end
+
+    true
+  end
+
+  def my_any?
+    for element in self
+      return true if yield(element)
+    end
+
+    false
+  end
+
+  def my_none?
+    for element in self
+      return false if yield(element)
+    end
+
+    true
+  end
+
+  def my_count
+    count = 0
+    for element in self
+      if block_given?
+        next unless yield(element)
+      end
+
+      count += 1
+    end
+
+    count
+  end
+
+  def my_map
+    arr = Array.new
+    for element in self
+      arr.push(yield(element))
+    end
+
+    arr
+  end
+
+  def my_inject(default = 0)
+    accumulator = default
+    for element in self
+      accumulator = yield(accumulator, element)
+    end
+
+    accumulator
+  end
 end
 
 # You will first have to define my_each
